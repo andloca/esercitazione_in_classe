7b69de895eec2097b8c8eb16aa7f5a25cdd4935f
andloca <119779474+andloca@users.noreply.github.com>
2026-03-10 21:15:20 +0100

    commit1

---
+++ helloword/ciao.java
@@ -0,0 +1,6 @@
+package helloword;
+public class ciao {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ variabili_e_tipizzazione_statica/variabili.java
@@ -0,0 +1,12 @@
+package variabili_e_tipizzazione_statica;
+public class variabili {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 3.14;
+        String c = "Ciao";
+
+    System.out.println("Valore di a: " + a);
+    System.out.println("Valore di b: " + b);
+    System.out.println("Valore di c: " + c);
+    }
+}
