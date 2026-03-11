40a7a72f1b58596d81411287d096eb28b67adb82
andloca <119779474+andloca@users.noreply.github.com>
2026-03-11 19:21:07 +0100

    Create tastera.java

---
+++ eserciziotre/tastera.java
@@ -0,0 +1,8 @@
+package eserciziotre;
+public class tastera {
+    public static void main(String[] args) {
+        String s=System.console().readLine("Inserisci il tuo nome: ");
+        String a= System.console().readLine(" Inserisci la tua età: ");
+        System.out.println("ciao " + s + " hai " + a + " anni");
+    }
+}
