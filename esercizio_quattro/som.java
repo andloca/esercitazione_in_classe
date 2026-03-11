015a6882f51b2c6a7eefc1ed0ac1c628a4325d41
andloca <119779474+andloca@users.noreply.github.com>
2026-03-11 19:29:25 +0100

    Create som.java

---
+++ esercizio_quattro/som.java
@@ -0,0 +1,12 @@
+package esercizio_quattro;
+
+public class som {
+    public static void main(String[] args){
+        int a = Integer.parseInt(System.console().readLine("Insersci il valore di a: "));
+        int b = Integer.parseInt(System.console().readLine("Insersci il valore di b: "));
+        System.out.println("Il risutalto è: "+ somma(a, b));
+    }
+    public static int somma(int a, int b){
+      return a+b;
+    }
+}
